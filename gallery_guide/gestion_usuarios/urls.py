b89779271056cc7b631ccc5d549b5b432f751499
Ramón Gavira <ramgavsan@alum.us.es>
2025-04-22 22:33:15 +0200

    feat: Estilos base y gestion de usuarios core

--- gallery_guide/gestion_usuarios/urls.py
+++ gallery_guide/gestion_usuarios/urls.py
@@ -14,10 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from gestion_usuarios import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        label="Nombre de Usuario",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre de usuario'})
+    )
+    password = forms.CharField(
+        label="Contraseña",
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Contraseña'})
+    )
 
 urlpatterns = [
-    path('', views.sample_http, name='sample_1')
+    path('registro/', views.registro, name='registro'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='login.html',
+        authentication_form=CustomAuthenticationForm
+    ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
